cfc1edf8efd078dcb267012280241ec0d1ac6aee
CodeOfVictor <60798818+CodeOfVictor@users.noreply.github.com>
2024-07-22 17:29:37 +0200

    Change Options visual

--- myprivacy/src/components/Options.js
+++ myprivacy/src/components/Options.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import { Box, Typography, Modal, Switch, Button, InputLabel, MenuItem, FormControl, Select, Stack } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const style = {
     position: 'absolute',
@@ -10,12 +9,23 @@ const style = {
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    border: '2px solid #1976d2',
     boxShadow: 24,
     p: 4,
 };
 
-export default function Options({ open, handleClose }) {
+function Options({ open, handleClose }) {
+    const [language, setLanguage] = React.useState('English');
+    const [darkMode, setDarkMode] = React.useState(true);
+
+    const handleChangeLanguage = (event) => {
+        setLanguage(event.target.value);
+    };
+
+    const handleChangeTheme = (event) => {
+        setDarkMode(event.target.checked);
+    };
+
     return (
         <Modal
             open={open}
@@ -25,12 +35,49 @@ export default function Options({ open, handleClose }) {
         >
             <Box sx={style}>
                 <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Text in a modal
+                    Options
                 </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography>
+
+                <Stack spacing={2} sx={{ mt: 2 }}>
+                    {/* Dark Theme Switch */}
+                    <Stack direction="row" alignItems="center" spacing={2}>
+                        <Typography>Dark Theme</Typography>
+                        <Switch checked={darkMode} onChange={handleChangeTheme} />
+                    </Stack>
+
+                    {/* Language Selector */}
+                    <Stack direction="row" alignItems="center" spacing={2}>
+                        <Typography>Language</Typography>
+                        <FormControl fullWidth>
+                            <InputLabel id="language-select-label">Language</InputLabel>
+                            <Select
+                                labelId="language-select-label"
+                                id="language-select"
+                                value={language}
+                                onChange={handleChangeLanguage}
+                                label="Language"
+                            >
+                                <MenuItem value={'English'}>English</MenuItem>
+                                <MenuItem value={'Spanish'}>Spanish</MenuItem>
+                                <MenuItem value={'German'}>German</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Stack>
+
+                    {/* Delete Button */}
+                    <Stack direction="row" alignItems="center" spacing={2}>
+                        <Typography>Delete All</Typography>
+                        <Button color="error" endIcon={<DeleteIcon />}>Delete</Button>
+                    </Stack>
+                </Stack>
+
+                {/* Save and Close Button */}
+                <Button variant="contained" color="primary" sx={{ mt: 3 }} onClick={handleClose}>
+                    Save and Close
+                </Button>
             </Box>
         </Modal>
     );
 }
+
+export default Options;
