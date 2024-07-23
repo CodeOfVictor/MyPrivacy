dddf938e7e96cd90ba567412f288d9100fd7e0ad
CodeOfVictor <60798818+CodeOfVictor@users.noreply.github.com>
2024-07-23 02:15:02 +0200

    Update App.js

--- myprivacy/src/App.js
+++ myprivacy/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/#/app" element={<Application />} />
+          <Route path="/app" element={<Application />} />
         </Routes>
       </Router>
     </Grid>
